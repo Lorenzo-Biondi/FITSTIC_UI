94baf04d0c156060f60877c4fe2e7be9cb62d76e
Lorenzo-Biondi <keithblond.mp3@gmail.com>
2024-11-20 11:44:13 +0100

    cambiato frontend

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.text('provaprova')
+def somma(l1:float, l2:float):
+    a = l1+l2
+    return a
 
-if st.checkbox('Check Box'):
-    st.write('checkkata')
+def main():
+    st.text('proviamo questo front-end')
+    num1 = st.slider('Inserisci lato1 rettandolo',0,100,2)
+    num2 = st.slider('Inserisci lato2 rettangolo',0,100,3)
+    r = somma(num1,num2)
+    if st.button('mostra risultato'):
+        st.write('la somma è', r)
+
+if __name__ == '__main__':
+    main()
